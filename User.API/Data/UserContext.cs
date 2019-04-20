de196a922cabe011142d733bdb4038441b1859d6
sigeshitou <sigeshitou@yeah.net>
2019-04-20 18:03:06 +0800

    初始化项目

---
+++ User.API/Data/UserContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using User.API.Models;
+
+namespace User.API.Data
+{
+    public class UserContext:DbContext
+    {
+
+        public UserContext(DbContextOptions<UserContext> options) :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppUser>().ToTable("Users").HasKey(u => u.Id);
+
+        }
+
+        public DbSet<AppUser> Users { get;set; }
+    }
+}
---
+++ User.API/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using User.API.Data;
+using User.API.Models;
+
+namespace User.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<UserContext>(option => 
+                option.UseMySQL(Configuration.GetConnectionString("MsqlUser")));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            IntitUserDataBase(app);
+        }
+
+        public void IntitUserDataBase(IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var userContext = scope.ServiceProvider.GetRequiredService<UserContext>();
+                if (userContext.Users.Any())
+                {
+                    userContext.Users.Add(new AppUser() {Name = "sigeshitou"});
+                    userContext.SaveChanges();
+                }
+            }
+        }
+    }
+}
